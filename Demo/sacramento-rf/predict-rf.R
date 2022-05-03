9be489c7f66313eb659ef314ca05fa3c7a018ca9
philbowsher <indyprb@gmail.com>
2022-05-03 13:47:18 +0000

    update demos

--- Demo/sacramento-rf/predict-rf.R
+++ Demo/sacramento-rf/predict-rf.R
@@ -1,13 +1,27 @@
 library(tidyverse)
 library(vetiver)
 
-endpoint <- vetiver_endpoint("http://ec2-18-216-69-78.us-east-2.compute.amazonaws.com/rsconnect/predict/")
-endpoint
+#step 3 use consumed model
+# endpoint <- vetiver_endpoint("http://ec2-18-216-69-78.us-east-2.compute.amazonaws.com/rsconnect/predict/")
+# endpoint
 
+#grab 20 random reconds and columns needed for predictions
 data(Sacramento, package = "modeldata")
 new_sac <- Sacramento %>%
     slice_sample(n = 20) %>%
     select(type, sqft, beds, baths)
 
-apiKey <- Sys.getenv("")
-predict(endpoint, new_sac, httr::add_headers(Authorization = paste("Key", apiKey)))
+new_sac
+
+vet_api_key <- Sys.getenv("VETIVER_API")
+# apiKey <- Sys.getenv("")
+
+# has to be the /predict endpoint at that specific API
+# vetiver_endpoint("http://ec2-18-216-69-78.us-east-2.compute.amazonaws.com/rsconnect/sacremento_rf_phil/predict")
+phil_endpoint <-  vetiver_endpoint(Sys.getenv("phil_endpoint_sac"))
+  
+
+# call it with auth (API key)
+res <- predict(phil_endpoint, new_sac, httr::add_headers(Authorization = paste("Key", vet_api_key)))
+
+new_sac %>% mutate(result = res)
--- Demo/sacramento-rf/train-rf.R
+++ Demo/sacramento-rf/train-rf.R
@@ -1,6 +1,7 @@
 library(tidymodels)
 data(Sacramento)
 
+#predict price for a bunch of random houses
 rf_spec <- rand_forest(mode = "regression")
 rf_form <- price ~ type + sqft + beds + baths
 
@@ -8,18 +9,22 @@ rf_fit <-
     workflow(rf_form, rf_spec) %>%
     fit(Sacramento)
 
+rf_fit
+
 library(vetiver)
 v <- vetiver_model(rf_fit, "sacramento_rf_phil")
 v
 
+#step2
 library(pins)
 model_board <- board_rsconnect(
     auth = "envvar",
-    server = "http://ec2-18-216-69-78.us-east-2.compute.amazonaws.com/rsconnect/", # Sys.getenv("CONNECT_SERVER")
+    server = Sys.getenv("CONNECT_SERVER"), # Sys.getenv("CONNECT_SERVER")
     account = "gt",
-    key = "")
+    key = Sys.getenv("VETIVER_API"))
 model_board %>% vetiver_pin_write(v)
 
+#plumber deploy
 vetiver_deploy_rsconnect(
     model_board,
     "gt/sacramento_rf_phil",
